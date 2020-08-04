03b8956dbe6c426b3ff52ea2fa32ae011bb303d3
Richa Sudikonda <richa@Richas-MacBook-Air.local>
2020-08-04 16:38:22 -0400

    Task 3: corrections

--- jobs/app.py
+++ jobs/app.py
@@ -6,10 +6,11 @@ PATH = "db/jobs.sqlite"
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g, _connection, None)
-    connection = g._connection = sqlite3.connection(PATH)
-    if row_factory(connection) is sqlite3.Row:
-        return connection
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connection(PATH)
+    connection.row_factory = sqlite3.Row:
+    return connection
 
 def execute_sql(sql, values = (), commit = False, single = False):
     connection = open_connection()
@@ -18,16 +19,17 @@ def execute_sql(sql, values = (), commit = False, single = False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
+
     cursor.close()
     return results
 
-def open_connection(exception):
-    connection = getattr(g, _connection, None)
-    if connection is None:
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
         connection.close()
-            
+
 @app.route('/')
 @app.route('/jobs')
-@app.teardown_appcontext
 def jobs():
     return render_template("index.html")
